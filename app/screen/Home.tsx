d84f96a7229c66b57f562e0d6ee1edfb6dbd5bb1
KohJisoo <js_koh@repurehc.com>
2024-10-08 16:25:37 +0900

    Feat:홈 스타일 변경

--- app/screen/Home.tsx
+++ app/screen/Home.tsx
@@ -1,46 +1,50 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
+import styled from "styled-components/native";
 
 type RootStackParamList = {
   Signin: { screen: string } | undefined;
 };
 
 export default function Home() {
-    
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
   return (
-      <View style={Styles.container}>
-        <Text style={Styles.HomeText}>홈페이지</Text>
-        <TouchableOpacity onPress={() => navigation.navigate("Signin", { screen: 'Signin' })} style={Styles.NextBottom}
-    >
-          <Text style={Styles.BottomText}>로그인 화면으로</Text>
-        </TouchableOpacity>
-      </View>
-  )
+    <S.Container>
+      <S.HomeText>홈페이지</S.HomeText>
+      <S.NextBottom
+        onPress={() => navigation.navigate("Signin", { screen: "Signin" })}
+      >
+        <Text>로그인 화면으로</Text>
+      </S.NextBottom>
+    </S.Container>
+  );
 }
 
-const Styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-    },
-    HomeText: {
-      fontSize: 30,
-      textAlign: "center",
-    },
-    NextBottom: {
-      backgroundColor: "purple",
-      padding: 10,
-      marginTop: "20%",
-      width: "50%",
-      alignSelf: "center",
-      borderRadius: 10,
-    },
-    BottomText: {
-      fontSize: 15,
-      color: 'white',
-      textAlign: "center",
-    }
-  })
+namespace S {
+  export const Container = styled.View`
+    flex: 1;
+    background-color: #fff;
+  `;
+
+  export const HomeText = styled.Text`
+    font-size: 30px;
+    text-align: center;
+  `;
+
+  export const NextBottom = styled.TouchableOpacity`
+    background-color: purple;
+    padding: 10px;
+    margin-top: 20%;
+    width: 50%;
+    align-self: center;
+    border-radius: 10px;
+  `;
+
+  export const BottomText = styled.Text`
+    font-size: 15px;
+    color: white;
+    text-align: center;
+  `;
+}
